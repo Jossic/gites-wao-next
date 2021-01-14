9b9d5b36f87dbab5affde88514ce29084087440d
Jossic <jossic.lapierre@gmail.com>
2021-01-14 20:59:11 +0100

    update giteModel

--- backend/models/giteModel.js
+++ backend/models/giteModel.js
@@ -1,15 +1,68 @@
 import mongoose from 'mongoose';
 const { ObjectId } = mongoose.Schema;
 
+const reviewSchema = mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		rating: { type: Number, required: true },
+		comment: { type: String, required: true },
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
 const giteSchema = mongoose.Schema(
 	{
 		nom: {
 			type: String,
 			unique: true,
 		},
-		images: {
+		mtitle: {
+			type: String,
+		},
+		mdesc: {
+			type: {},
+		},
+		logoGite: {
 			data: Buffer,
 			contentType: String,
+		},
+		imagesCarrousel: {
+			data: Buffer,
+			contentType: String,
+		},
+		imagesCarrousel: {
+			data: Buffer,
+			contentType: String,
+		},
+		couleur1: {
+			type: String,
+		},
+		couleur2: {
+			type: String,
+		},
+		vidéoLink: {
+			type: String,
+		},
+		texte1: {
+			type: {},
+			min: 20,
+			max: 2000000,
+		},
+		detailGite: {
+			type: {},
+			min: 20,
+			max: 2000000,
+		},
+		reviews: [reviewSchema],
+		capacite: {
+			type: Number,
 		},
 	},
 	{
