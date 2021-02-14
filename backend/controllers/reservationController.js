12846e1d3675de8ce9d54e820dca741ce6ec1fb8
Jossic <jossic.lapierre@gmail.com>
2021-02-14 14:34:09 +0100

    Back - clientRoute

---
+++ backend/controllers/reservationController.js
@@ -0,0 +1,126 @@
+import Reservation from '../models/ReservationModel.js';
+import asyncHandler from 'express-async-handler';
+import validateHuman from '../utils/validateHuman.js';
+
+// @desc      Fetch all reservations
+// @route     GET /api/reservation
+// @access    Public
+const getAllReservations = asyncHandler(async (req, res) => {
+	const reservations = await Reservation.find({});
+	res.json(reservations);
+});
+
+// @desc      Fetch one Reservation by Id
+// @route     GET /api/Reservation/:id
+// @access    Public
+const getReservationById = asyncHandler(async (req, res) => {
+	const reservation = await Reservation.findById(req.params.id);
+
+	if (reservation) {
+		res.json(reservation);
+	} else {
+		res.status(404);
+		throw new Error('Reservation non trouvé');
+	}
+});
+
+// @desc      Create a Reservation
+// @route     POST /api/Reservation
+// @access    Private/Admin
+const createReservation = async (req, res) => {
+	const { nom, mail, msg, tel, vu, token } = req.body;
+
+	const human = await validateHuman(token);
+	if (!human) {
+		res.status(400);
+		res.json({ error: 'Vous avez été reconnu en tant que robot' });
+		return;
+	}
+
+	const reservation = new Reservation({
+		nom,
+		mail,
+		msg,
+		tel,
+		vu,
+		token,
+	});
+
+	console.log('Reservation dans le back', reservation);
+
+	reservation.save((error, reservation) => {
+		if (error) return res.status(400).json({ error });
+		if (reservation) {
+			res.status(201).json({
+				reservation,
+				text:
+					'Votre reservation à bien été envoyé, nous reviendrons vers vous rapidement, redirection en cours...',
+			});
+		}
+	});
+};
+
+// @desc      Delete a reservation
+// @route     GET /api/reservation
+// @access    Private/Admin
+const removeReservation = asyncHandler(async (req, res) => {
+	const reservation = await Reservation.findById(req.params.id);
+	if (reservation) {
+		await reservation.remove();
+		res.json({
+			text: 'Reservation correctement supprimé',
+		});
+	} else {
+		return res.json({
+			error: err,
+		});
+	}
+});
+
+// @desc      Add a response
+// @route     PUT /api/reservation/:id
+// @access    Private/Admin
+const updateReservation = asyncHandler(async (req, res) => {
+	const { reponse } = req.body;
+
+	const reservation = await Reservation.findById(req.params.id);
+	console.log('reservation apres requete', reservation);
+	if (reservation) {
+		reponse && (reservation.reponse = reponse);
+		reservation.dateReponse = Date.now();
+		reservation.repondu = true;
+		console.log('reservation avant save', reservation);
+
+		const updatedreservation = await reservation.save();
+		res.json({
+			updatedreservation,
+			text: 'Le mail a bien été envoyé, une copie vous sera envoyée',
+		});
+	} else {
+		res.status(404);
+		throw new Error('Reservation non trouvée');
+	}
+});
+// @desc      Set Vu = true
+// @route     PUT /api/reservation/:id/vu
+// @access    Private/Admin
+const setVu = asyncHandler(async (req, res) => {
+	const reservation = await Reservation.findById(req.params.id);
+	if (reservation) {
+		reservation.vu = true;
+
+		const updatedReservation = await reservation.save();
+		res.json(updatedReservation);
+	} else {
+		res.status(404);
+		throw new Error('Reservation non trouvée');
+	}
+});
+
+export {
+	createReservation,
+	getAllReservations,
+	getReservationById,
+	removeReservation,
+	updateReservation,
+};
---
+++ backend/routes/clientRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const router = express.Router();
+import {
+	getAllClients,
+	getClientById,
+	createClient,
+	removeClient,
+	updateClient,
+} from '../controllers/clientController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+//Public
+// router.post('/client', createClient); //Inutile car créé avec reservation
+
+//Admin
+router.get('/client', protect, admin, getAllClients);
+router.get('/client/:id', protect, admin, getClientById);
+router.delete('/client/:id', protect, admin, removeClient);
+router.put('/client/:id', protect, admin, updateClient);
+
+export default router;
